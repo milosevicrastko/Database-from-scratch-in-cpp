7f43a786b556bc4501136b85b4f29cbb816e9796
rastko <milosevic.rastko@yahoo.com>
2024-09-03 20:42:37 +0200

    Added socket to server

--- database-from-scratch/server.cpp
+++ database-from-scratch/server.cpp
@@ -1,7 +1,10 @@
 #include<iostream>
+#include<sys/socket.h>
+#include<arpa/inet.h>
 #include "server.h"
+#include <unistd.h>
 
-Server::Server(std::string ipAddress, std::uint16_t port) {
+Server::Server(std::string ipAddress, std::uint16_t port):sock(0) {
     this->ipAddress = ipAddress;
     this->port = port;
 
@@ -13,11 +16,17 @@ Server::~Server() {
 
 
 int Server::start(){
+    this->sock = socket(AF_INET, SOCK_STREAM,0);
+    if (this->sock < 0){
+        std::cout<<"Cannot create socket"<<"\n";
+        return 1;
+    }
     std::cout<<"Server started on address "<<ipAddress<<" and on port " <<port<< "\n";
     return 0;
 }
 
 void Server::stop() {
+    close(sock);
     std::cout<<"Server stopped"<<"\n";
 }
 
--- database-from-scratch/server.h
+++ database-from-scratch/server.h
@@ -4,6 +4,7 @@ class Server {
     private:
         std::string ipAddress;
         std::uint16_t port;
+        int sock;
     public:
         Server(std::string ipAddress, std::uint16_t port);
         ~Server();
